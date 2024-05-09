34e3d2f9bdabb3e8c274f0ca6c0e201497da92ed
Fangqing Xia <fangqingxia1994@tamu.edu>
2024-05-09 18:36:29 -0500

    Init

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import { AnimatePresence } from 'framer-motion';
+import { lazy, Suspense, useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import AppFooter from './components/shared/AppFooter';
+import AppHeader from './components/shared/AppHeader';
+import bgDay from './images/JPstreet-day.jpg';
+import bgNight from './images/JPstreet-night.jpeg';
+import useThemeSwitcher from './hooks/useThemeSwitcher';
+import './css/App.css';
+import UseScrollToTop from './hooks/useScrollToTop';
+
+const About = lazy(() => import('./pages/AboutMe'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+const Home = lazy(() => import('./pages/Home'));
+const Projects = lazy(() => import('./pages/Projects'));
+const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
+
+
+function App() {
+	const [activeTheme, setTheme] = useThemeSwitcher();
+	const [backgroundImage, setBackgroundImage] = useState(activeTheme === 'dark' ? bgDay : bgNight);
+	const [overlayColor, setOverlayColor] = useState(activeTheme === 'dark' ? 'bg-primary-light' : 'bg-gray-700');
+
+	 // Function to handle theme change
+	const handleThemeChange = (newTheme) => {
+		setBackgroundImage(newTheme === 'dark' ? bgDay : bgNight);
+		setOverlayColor(newTheme === 'dark' ? 'bg-primary-light' : 'bg-gray-700');
+	};
+
+	return (
+		<AnimatePresence>
+			<div className="relative h-screen">
+				<div>
+                    <img src={backgroundImage} className="fixed h-full w-full z-0" />
+					
+                </div> 
+				
+				<div className={`absolute inset-0 mx-auto w-full sm:w-85/100 flex items-center bg-opacity-80 ${overlayColor} z-10`}></div>
+				
+				<div className="relative w-full h-full z-20">
+					<Router>
+						<ScrollToTop />
+						<AppHeader onThemeChange={handleThemeChange}/>
+						<Suspense fallback={""}>
+						
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="projects" element={<Projects />} />
+							<Route
+								path="projects/single-project"
+								element={<ProjectSingle />}
+							/>
+							<Route path="about" element={<About />} />
+							<Route path="contact" element={<Contact />} />
+						</Routes>
+						</Suspense>
+						<AppFooter />
+					</Router>
+					<UseScrollToTop />
+				</div>
+				
+			</div>
+		</AnimatePresence>
+	);
+}
+
+export default App;
---
+++ src/components/projects/ProjectSingle.jsx
@@ -0,0 +1,41 @@
+import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+
+const ProjectSingle = ({ title, category, image }) => {
+	return (
+		<motion.div
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1, delay: 1 }}
+			transition={{
+				ease: 'easeInOut',
+				duration: 0.7,
+				delay: 0.15,
+			}}
+		>
+			<Link to="/projects/single-project" aria-label="Single Project">
+				<div className="group relative overflow-hidden rounded-xl shadow-lg cursor-pointer mb-10 sm:mb-0 hover:shadow-xl">
+					<div className="group-hover:bg-black opacity-70 w-full h-full absolute z-40 transition-all duration-300"></div>
+					<div>
+						<img
+							src={image}
+							className="group-hover:scale-125 transition-all duration-500 rounded-t-xl border-none"
+							alt="Single Project"
+						/>
+					</div>
+					<div className="absolute bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+						<span className="font-general-medium text-lg md:text-xl text-ternary-light mb-2">
+							{title}
+						</span>
+					</div>
+					<div className="absolute bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+						<span className="text-lg text-ternary-light">
+							{category}
+						</span>
+					</div>
+				</div>
+			</Link>
+		</motion.div>
+	);
+};
+
+export default ProjectSingle;
