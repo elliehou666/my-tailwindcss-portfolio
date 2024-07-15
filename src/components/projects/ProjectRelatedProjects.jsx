2d05236193e90a09002b24fa65a226c386de20e4
Fangqing Xia <fangqingxia1994@tamu.edu>
2024-07-15 14:51:43 -0500

    feat: hide relatedProjects component when a project has no related project

--- src/components/projects/ProjectRelatedProjects.jsx
+++ src/components/projects/ProjectRelatedProjects.jsx
@@ -4,6 +4,11 @@ const ProjectRelatedProjects = ({projectId}) => {
 	const { findProjectById } = useSingleProjectContext();
 	const targetProject = findProjectById(projectId);
 
+	// Check if there are no related projects
+	if (targetProject.RelatedProject.Projects.length === 0) {
+		return null;
+	}
+
 	return (
 		<div className="mt-10 pt-10 sm:pt-14 sm:mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
 			<p className="font-general-regular text-primary-dark dark:text-primary-light text-3xl font-bold mb-10 sm:mb-14 text-left">
