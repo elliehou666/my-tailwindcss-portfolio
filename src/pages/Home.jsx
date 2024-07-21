66dfe45d2cbe7e8b633c2d68b6f31f044408ebae
Fangqing Xia <fangqingxia1994@tamu.edu>
2024-07-21 10:46:40 -0500

    fix: change a tag to Link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,16 +26,15 @@ const Home = () => {
 			<div className="mt-8 sm:mt-10 sm:w-2/3 w-64 flex flex-col">
 					<p className='text-ternary-dark dark:text-ternary-light text-md test-justify'>To delve deeper into my work and explore the innovative projects I've been involved in, I invite you to visit my Projects page. There, you’ll find detailed descriptions, technical insights, and the impact of my various projects.</p>
 
-				<a
-						href="/projects"
-						className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-						aria-label="Explore projects"
-					>
+				<Link
+					to="/projects"
+					className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+				>
 						<FiLink2 className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiLink2>
 						<span className="text-sm sm:text-lg font-general-medium duration-100">
 							Explore Projects
 						</span>
-					</a>
+				</Link>
 			</div>
 
 			<div className="sm:w-1/3 mb-7 sm:mb-0">
